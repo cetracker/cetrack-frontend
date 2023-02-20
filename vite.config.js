1b2f03181a33c385878afa3196bfd2ea0894768c
Meinhard Ritscher <unreachable@gmx.net>
2023-02-20 20:21:05 +0100

    Add eslint config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 // https://stackoverflow.com/questions/66043612/vue3-vite-project-alias-src-to-not-working
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    { // default settings on build (i.e. fail on error)
+      ...eslint(),
+      apply: 'build',
+    },
+    { // do not fail on serve (i.e. local development)
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enforce: 'post'
+  }],
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
