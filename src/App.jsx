4de66f9711634e49e308036ca8eaef38f9f58e32
Meinhard Ritscher <unreachable@gmx.net>
2023-02-21 15:13:13 +0100

    Temporarily add Mantine examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { Badge, Code, Flex, MantineProvider, Text } from '@mantine/core';
+import ElementBox from './components/ElementBox/ElementBox';
+
+const theme = {
+  // Override any other properties from default theme
+  colorScheme: 'light',
+  fontFamily: 'Open Sans, sans serif',
+  spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: 30 },
+    },
+  },
+}
 
 const App = () => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Great things to come.</Text>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <Flex mih={30} justify="center" align="center" bg="gray.2" gap="md" >
+        <Text>Great things to come.</Text>
+        <Badge size='md'>Badge</Badge>
+      </Flex>
+      <Code>
+        Code snippet
+      </Code>
+      <ElementBox />
     </MantineProvider>
   )
 }
