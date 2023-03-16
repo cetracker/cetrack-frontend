1f8047bd3129dab92e1283805dafc0e307660a3f
Meinhard Ritscher <unreachable@gmx.net>
2023-03-16 17:12:41 +0100

    Improve main error page

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,34 +1,23 @@
-import { Button, Container, createStyles, Group, Text, Title } from '@mantine/core';
+import { Button, Code, Container, createStyles, Group, Text, Title } from '@mantine/core';
 import { IconArrowLeft } from '@tabler/icons-react';
 import { Link, useRouteError } from 'react-router-dom';
 
 const useStyles = createStyles((theme) => ({
   root: {
-    paddingTop: 80,
-    paddingBottom: 80,
+    paddingTop: 40,
+    paddingBottom: 40,
   },
 
   label: {
     textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 220,
+    fontWeight: 800,
+    fontSize: 120,
     lineHeight: 1,
     marginBottom: theme.spacing.xl * 1.5,
     color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
 
     [theme.fn.smallerThan('sm')]: {
-      fontSize: 120,
-    },
-  },
-
-  title: {
-    fontFamily: `${theme.fontFamily}`,
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 38,
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 32,
+      fontSize: 80,
     },
   },
 
@@ -36,31 +25,52 @@ const useStyles = createStyles((theme) => ({
     maxWidth: 500,
     margin: 'auto',
     marginTop: theme.spacing.xl,
-    marginBottom: theme.spacing.xl * 1.5,
+    marginBottom: theme.spacing.xl * 1.1,
   },
 }));
 
 const NotFoundPage = () => {
   const { classes } = useStyles();
   const error = useRouteError();
-  console.error(error)
 
   return (
-    <Container className={classes.root}>
-      <div className={classes.label}>404</div>
-      <Title className={classes.title}>You have found a secret place.</Title>
-      <Text color="dimmed" size="lg" align="center" className={classes.description}>
-        Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
-        been moved to another URL.
-      </Text>
-      <Text color="red" size="xs" align="center">
-        {error.statusText || error.message}
-      </Text>
+    <Container className={classes.root} align="center" >
+      <div className={classes.label}>{error.response.status}</div>
+      { error.response.status >=500 ? (
+        <>
+          <Title order={4}>This is an unexpected internal error.</Title>
+          <Text color="dimmed" size="lg" align="center" className={classes.description}>
+            Please try again shortly.
+          </Text>
+        </>
+      ) : (
+        <>
+          <Title order={4}>You have found a secret place.</Title>
+          <Text color="dimmed" size="lg" align="center" className={classes.description}>
+            Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
+            been moved to another URL.
+          </Text>
+          <Text color="red" size="xs" align="center">
+            {error.statusText || error.message}
+          </Text>
+        </>
+      )}
       <Group position="center">
         <Button component={Link} to ="/" size="md" leftIcon={<IconArrowLeft size={16} stroke={1.5} />}>
           Take me back to home page
         </Button>
       </Group>
+      { error.response?.data ? (
+        <Code block={true}>
+          {error.response.data.timestamp}
+          {'\n'}
+          {error.response.data.status} {error.response.data.error}
+          {'\n'}
+          {error.response.data.message}
+          {'\n'}
+          {error.response.data.path}
+        </Code>
+      ):(<></>)}
     </Container>
   );
 }
