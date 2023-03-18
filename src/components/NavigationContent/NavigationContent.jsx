568b4dde91ac145306ae8010fddaa4db5a52330c
Meinhard Ritscher <unreachable@gmx.net>
2023-03-18 18:49:16 +0100

    Improve Navigation design

--- src/components/NavigationContent/NavigationContent.jsx
+++ src/components/NavigationContent/NavigationContent.jsx
@@ -1,22 +1,57 @@
-import { Button, Flex } from '@mantine/core';
+import { Box, NavLink } from '@mantine/core';
+import { IconAB2, IconBike, IconMap, IconSettingsFilled, IconUpload } from '@tabler/icons-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavigationContent = () => {
+  const [active, setActive] = useState('')
   return (
-    <Flex direction="column" gap="xs" align="center" justify="flex-start">
-      <Button component={Link} to="/parts">
-        Parts
-      </Button>
-      <Button component={Link} to="/partTypes">
-        Part Types
-      </Button>
-      <Button component={Link} to="/bikes">
-        Bikes
-      </Button>
-      <Button component={Link} to="/tours">
-        Tours
-      </Button>
-    </Flex>
+    <Box sx={{ width: 150 }}>
+      <NavLink
+        label="Parts"
+        component={Link} to="/parts"
+        icon={<IconSettingsFilled />}
+        onClick={() => setActive('parts')}
+        active={'parts' === active}
+        color="teal"
+        variant='filled'
+      />
+      <NavLink
+        label="Part Types"
+        component={Link} to="/partTypes"
+        onClick={() => setActive('parttypes')}
+        icon={<IconAB2 />}
+        active={'parttypes' === active}
+        color="teal"
+        variant='filled'
+      />
+      <NavLink
+        label="Bikes"
+        component={Link} to="/bikes"
+        icon={<IconBike />}
+        onClick={() => setActive('bikes')}
+        active={'bikes' === active}
+        color="teal"
+        variant='filled'
+      />
+      <NavLink
+        label="Tours"
+        component={Link} to="/tours"
+        icon={<IconMap />}
+        onClick={() => setActive('tours')}
+        active={'tours' === active}
+        color="teal"
+        variant='filled'/>
+      <NavLink
+        label="Import Tours"
+        component={Link} to="/tourImport"
+        icon={<IconUpload />}
+        onClick={() => setActive('tourimport')}
+        active={'tourimport' === active}
+        color="teal"
+        variant='filled'
+      />
+    </Box>
   );
 }
 
