b18912923b279b9e4af4aaac7bb5e579a75440d0
Meinhard Ritscher <unreachable@gmx.net>
2024-09-13 13:45:45 +0200

    Context path was missing on backend

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '^/mock/api': {
         target: ''
