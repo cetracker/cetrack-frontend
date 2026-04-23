f1fcb6c831036ee575abd71c6ac71a861e68de0d
Meinhard Ritscher <unreachable@gmx.net>
2026-04-23 14:40:42 +0200

    Fresh start for V2

--- src/helper/dateCompare.js
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-export const compareDates = (dayA, dayB) => {
-
-  if (dayA === '') return 1
-  if (dayB === '') return -1
-
-  return dayjs(dayB).isBefore(dayjs(dayA)) ? 1 : -1
-}
